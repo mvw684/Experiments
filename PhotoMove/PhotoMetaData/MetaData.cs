5525fba81857f56f27d58db291fbaff079e7fb26
Mark van Wijk <mvw684@users.noreply.github.com>
2019-11-23 19:20:07 +0100

    Adding initial files

---
+++ PhotoMove/PhotoMetaData/MetaData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoMetaData {
+    public class MetaData {
+        private FileInfo file;
+        public MetaData(FileInfo file) {
+            this.file = file;
+        }
+    }
+}
