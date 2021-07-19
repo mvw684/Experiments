65bd57cbd3e5094c398022477c6cb075f2693e9d
Mark van Wijk <mvw684@users.noreply.github.com>
2021-07-19 10:28:34 +0200

    Batch file experiments

---
+++ BatchFile/a.cmd
@@ -0,0 +1,19 @@
+pushd "%~dp0"
+set location %~dp0
+set name %~n0
+@echo %DATA% %TIME%
+set EXIT_CODE=0
+
+call log a a a
+
+goto :SUCCESS
+:ERROR
+set EXIT_CODE=1
+echo %0: exited with error %1
+echo %0: %1
+
+:SUCCESS
+popd
+
+@echo %DATA% %TIME%
+exit /b %EXIT_CODE%
